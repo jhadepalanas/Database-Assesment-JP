bc8ca125e56c9afde40d8c644a2fca415b3eb66a
Jhade Palanas <jhadepalanas@riccarton.school.nz>
2024-07-04 09:05:44 +1200

    menu choice added

--- menu_choicE.py
+++ menu_choicE.py
@@ -1,27 +1,3 @@
-# Import the libraries to connect to the database and present the information in tables
-import sqlite3
-from tabulate import tabulate
-
-# This is the filename of the database to be used
-DB_NAME = 'netflix_series.db'
-
-def print_query(view_name:str):
-    ''' Prints the specified view from the database in a table '''
-    # Set up the connection to the database
-    db = sqlite3.connect(DB_NAME)
-    cursor = db.cursor()
-    # Get the results from the view
-    sql = "SELECT * FROM '" + view_name + "'"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    # Get the field names to use as headings
-    field_names = "SELECT name from pragma_table_info('" + view_name + "') AS tblInfo"
-    cursor.execute(field_names)
-    headings = list(sum(cursor.fetchall(),()))
-    # Print the results in a table with the headings
-    print(tabulate(results,headings))
-    db.close()
-
 menu_choice =''
 while menu_choice !='z':
     menu_choice = input('Welcome to the Netflix Series database\n\b'
@@ -61,3 +37,4 @@ while menu_choice !='z':
         print_query('speaks English')
     if menu_choice == 'K':
         print_query('speaks Korean')
+    
